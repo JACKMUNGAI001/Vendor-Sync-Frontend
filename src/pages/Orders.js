b973ae593360b345dad76e4227b4158dd766f8ca
waynetravis <waynetavis123@gmail.com>
2025-10-24 20:48:28 +0300

    Create Orders page with responsive layout and OrderList component

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,1 +1,14 @@
-// Orders page will be implemented here
+import React from 'react';
+import OrderList from '../components/OrderList';
+
+function Orders() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <OrderList />
+      </div>
+    </div>
+  );
+}
+
+export default Orders;

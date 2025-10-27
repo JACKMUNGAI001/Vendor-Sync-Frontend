0e0c9f5017ceb6a88deaa337788ca6f84f581f86
Mkikii <maureenwkarimi@gmail.com>
2025-10-27 05:09:06 +0300

    style: improve NewOrder page layout and styling

--- src/pages/NewOrder.js
+++ src/pages/NewOrder.js
@@ -3,10 +3,8 @@ import OrderForm from '../components/OrderForm';
 
 function NewOrder() {
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <OrderForm />
-      </div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <OrderForm />
     </div>
   );
 }
